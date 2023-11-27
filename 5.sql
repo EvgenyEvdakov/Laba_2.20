377fd8e4fedfe31857ee870dc635ba036f2dbb77
Евгений <125446285+EvgenyEvdakov@users.noreply.github.com>
2023-11-27 10:41:25 +0300

    Update 5.sql

--- 5.sql
+++ 5.sql
@@ -1,13 +1,6 @@
-sqlite> .import --csv city.csv city
-sqlite> select timezone, COUNT(*) as city_count
-   ...> from city
-   ...> where federal_district in ('Сибирский', 'Приволжский')
-   ...> group by timezone
-   ...> order by timezone;
-UTC+3|101
-UTC+4|41
-UTC+5|58
-UTC+6|6
-UTC+7|86
-UTC+8|22
-sqlite>
+.import --csv city.csv city
+select timezone, COUNT(*) as city_count
+from city
+where federal_district in ('Сибирский', 'Приволжский')
+group by timezone
+order by timezone;
