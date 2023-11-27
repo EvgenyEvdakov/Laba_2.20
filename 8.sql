ee31f4df0b0a7cc104c76b05f50b28136acad8a6
Евгений <125446285+EvgenyEvdakov@users.noreply.github.com>
2023-11-27 10:43:01 +0300

    Update 8.sql

--- 8.sql
+++ 8.sql
@@ -1,21 +1,9 @@
-sqlite> .import --csv  city.csv city
-sqlite> .headers on
-sqlite> .mode csv
-sqlite> .separator |
-sqlite> SELECT timezone, COUNT(*) AS city_count
-   ...> FROM city
-   ...> GROUP BY timezone
-   ...> ORDER BY city_count DESC;
-timezone|city_count
-UTC+3|660
-UTC+5|173
-UTC+7|86
-UTC+4|66
-UTC+9|31
-UTC+8|28
-UTC+2|22
-UTC+10|22
-UTC+11|17
-UTC+6|6
-UTC+12|6
-sqlite> 
+.import --csv  city.csv city
+.headers on
+.mode csv
+.separator |
+SELECT timezone, COUNT(*) AS city_count
+FROM city
+GROUP BY timezone
+ORDER BY city_count DESC;
+
